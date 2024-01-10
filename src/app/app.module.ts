af34805204a07270f3b825b71e159566156c4507
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-10 12:35:52 -0500

    STEP 24 : assignment completion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-success-alert',
+  template: `<p>Success Works!</p>`,
+  styles: `p { color: darkGreen}`,
+})
+export class SuccessAlertComponent {}
