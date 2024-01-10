da168cf1904289608488b115110a59de0bef026d
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-10 12:16:21 -0500

    STEP 23  using styles instead of stylesUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  //  styleUrl: './app.component.css',
+  styles: [
+    `
+      h3 {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   name = 'first-angular-app2';
