0111c0fcb6c0c91581b82cf5a6657ad1b9216c20
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-10 12:01:33 -0500

    STEP 20 (2) replace single quote in template with back tick

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  template: `<app-server></app-server>
+    <app-server></app-server>
+    <app-server></app-server>
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrl: './servers.component.css',
 })
 export class ServersComponent {}
