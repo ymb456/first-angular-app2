9118832abf78f722b03650936836ac1799fcd7b9
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-12 09:28:04 -0500

    STEP 28

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `<app-server></app-server>
+  /*  template: `<app-server></app-server>
     <app-server></app-server>
     <app-server></app-server>
     <app-server></app-server>
-    <app-server></app-server>`,
+    <app-server></app-server>`,*/
+  templateUrl: 'servers.component.html',
   styleUrl: './servers.component.css',
 })
-export class ServersComponent {}
+export class ServersComponent implements OnInit {
+  allowAddServer: boolean = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowAddServer = true;
+    }, 2000);
+  }
+  ngOnInit(): void {}
+}
